323bda2714d24708f9be4086bfdfb55150e5cf56
Bruna Ferreira <brunagaby8@gmail.com>
2025-04-01 10:07:38 -0300

    adição de atributos que controlam a capacidade máxima de casas  atendidas

--- src/model/Poste.java
+++ src/model/Poste.java
@@ -1,21 +1,20 @@
 package model;
 
 public class Poste {
-    private static int contador = 0; 
+    private static int contador = 0; //conta quantos postes tem
+    private static int capacidadeMax = 5; //qntd máxima de casas de clientes para aquele poste
     private String id;
-    private boolean conectado;
+    private boolean conectado; //se o poste está conectao a provedora
+    private int casasAtendidas; //quantidade de casas atuais, não deve ultrapassar capacidadeMax
 
     public Poste(boolean conectado){
         this.id = String.valueOf(contador++);
         this.conectado = conectado;
+        this.casasAtendidas = 0;
     }
 
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public boolean isConectado() {
@@ -25,4 +24,21 @@ public class Poste {
     public void setConectado(boolean conectado) {
         this.conectado = conectado;
     }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public static int getCapacidadeMax() {
+        return capacidadeMax;
+    }
+
+    public int getCasasAtendidas() {
+        return casasAtendidas;
+    }
+
+    public void setCasasAtendidas(int casasAtendidas) {
+        this.casasAtendidas = casasAtendidas;
+    }
+
 }
