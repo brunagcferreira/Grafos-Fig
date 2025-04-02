9e2332dc85aececfc1ffe8fc5b327a80334ffb87
Jorge Ribeiro <102694110+JorgRibeiro@users.noreply.github.com>
2025-04-02 19:08:37 -0300

    Teste

--- src/test/Teste.java
+++ src/test/Teste.java
@@ -4,25 +4,38 @@ import model.Conexoes;
 import model.Poste;
 import model.RedeEletrica;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import util.LeitorArquivo;
 
-public class Test {
+import java.util.LinkedList;
+import java.util.List;
+
+public class Teste {
 
     private RedeEletrica rede;
     private BuscaEmLargura busca;
 
-     @BeforeEach
-    void setUp() throws Exception {
+    @BeforeEach
+    void setUp() {
+
         String entrada = "src/resources/entrada_teste.txt";  // Arquivo de testes
         LeitorArquivo leitor = new LeitorArquivo(entrada);
         leitor.inicializarListaDeAdjacencia();
 
         rede = new RedeEletrica(leitor.getListaDeAjacencia(), leitor.getListaConexoes(), leitor.getListaPostes());
         busca = new BuscaEmLargura(rede);
-        
+
         // Define um poste como conectado (simulando a provedora)
-        Poste provedora = rede.encontrarPostePorId("0");
+        Poste provedora = rede.encontrarPostePorId("2");
         provedora.setConectado(true);
+    }
+    @Test
+    public void conectar_poste() throws Exception {
+
+        BuscaEmLargura buscaLargura = new BuscaEmLargura(rede);
+        List<Poste> caminho = buscaLargura.encontrarCaminho(rede.getListaPostes().getFirst());
+        buscaLargura.conectarCaminho(caminho);
+        assert rede.getListaPostes().getFirst().getCasasAtendidas() > 0;
     }
 
 
