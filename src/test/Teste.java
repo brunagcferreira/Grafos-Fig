eecdc4a9b3b8e12c3930f6f6578a303db777f568
Jorge Ribeiro <102694110+JorgRibeiro@users.noreply.github.com>
2025-04-03 16:47:58 -0300

    Testes

--- src/test/Teste.java
+++ src/test/Teste.java
@@ -7,32 +7,70 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import util.LeitorArquivo;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Teste {
 
     private RedeEletrica rede;
     private BuscaEmLargura busca;
 
+    private ArrayList<Poste> listaPostes = new ArrayList<>();
+    private ArrayList<Conexoes> listaConexoes = new ArrayList<>();
+    private Map<String, LinkedList<String>> listaDeAjacencia = new LinkedHashMap<String, LinkedList<String>>();
+
+    @Test
+    public void criar_poste() throws Exception {
+        Poste novo_poste = new Poste(false);
+        listaPostes.add(novo_poste);
+        listaDeAjacencia.put(novo_poste.getId(), null);
+        assert listaPostes.contains(novo_poste);
+    }
+
+    @Test
+    public void criar_conexao() throws Exception {
+        Poste poste_destino = new Poste(false);
+        Poste poste_origem = new Poste(false);
+        listaPostes.add(poste_destino);
+        listaPostes.add(poste_origem);
+
+        LinkedList<String> poste_destino_adj = new LinkedList<>();
+        poste_destino_adj.add(poste_origem.getId());
+
+        listaDeAjacencia.put(poste_destino.getId(), poste_destino_adj);
+
+        Conexoes nova_conexao = new Conexoes(poste_origem,poste_destino,8.0);
+        listaConexoes.add(nova_conexao);
+
+        assert listaConexoes.contains(nova_conexao);
+    }
+
+    @Test
+    public void criar_rede() throws Exception {
+        RedeEletrica rede_teste = new RedeEletrica(listaDeAjacencia,listaConexoes,listaPostes);
+        assert  rede_teste.getListaPostes().equals(listaPostes) &&
+                rede_teste.getListaConexoes().equals(listaConexoes) ;
+    }
+
     @BeforeEach
     void setUp() {
-
-        String entrada = "src/resources/entrada_teste.txt";  // Arquivo de testes
-        LeitorArquivo leitor = new LeitorArquivo(entrada);
-        leitor.inicializarListaDeAdjacencia();
-
-        rede = new RedeEletrica(leitor.getListaDeAjacencia(), leitor.getListaConexoes(), leitor.getListaPostes());
+/*
+        rede = new RedeEletrica(listaDeAjacencia, listaConexoes, listaPostes);
         busca = new BuscaEmLargura(rede);
 
-        // Define um poste como conectado (simulando a provedora)
-        Poste provedora = rede.encontrarPostePorId("2");
-        provedora.setConectado(true);
+
+*/
     }
+
     @Test
     public void conectar_poste() throws Exception {
+        RedeEletrica rede = new RedeEletrica(listaDeAjacencia,listaConexoes,listaPostes);
+
+        // Define um poste como conectado (simulando a provedora)
+        Poste provedora = rede.encontrarPostePorId("1");
+        provedora.setConectado(true);
 
         BuscaEmLargura buscaLargura = new BuscaEmLargura(rede);
+
         List<Poste> caminho = buscaLargura.encontrarCaminho(rede.getListaPostes().getFirst());
         buscaLargura.conectarCaminho(caminho);
         assert rede.getListaPostes().getFirst().getCasasAtendidas() > 0;
